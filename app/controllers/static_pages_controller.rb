877162b068522511c71907e2a7c5a8ecce2da4e4
Josef Zacek <info@josefzacek.com>
2015-10-19 00:17:41 +0100

    edit, update action implemented

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,26 @@
 class StaticPagesController < ApplicationController
-  def home_page
-    @message = StaticPage.current_time_and_date
-    @content = StaticPage.find(1)
+  def show
+    @time = StaticPage.current_time_and_date
+    @page = StaticPage.find(params[:id])
+  end
+
+  def edit
+    @page = StaticPage.find(params[:id])
+  end
+
+  def update
+    @page = StaticPage.find(params[:id])
+
+    if @page.update_attributes(page_parameters)
+      redirect_to action: 'show', id: @page
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def page_parameters
+    params.require(:static_page).permit(:url, :content)
   end
 end
