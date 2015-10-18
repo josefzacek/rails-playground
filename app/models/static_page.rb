01a26573ae7266511ecf427c42581e5800927d2f
Josef Zacek <info@josefzacek.com>
2015-10-19 00:20:55 +0100

    added url column to StaticPages table

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -6,6 +6,7 @@
 #  content    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  url        :string
 #
 
 class StaticPage < ActiveRecord::Base
--- test/models/static_page_test.rb
+++ test/models/static_page_test.rb
@@ -6,6 +6,7 @@
 #  content    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  url        :string
 #
 
 require 'test_helper'
